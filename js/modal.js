c834378bf8b0737549cbe7875e9b7b816e8947e5
samtelk <studyhaliln@gmail.com>
2023-07-09 20:08:02 +0400

    fix

---
+++ js/modal.js
@@ -0,0 +1,22 @@
+let images = document.querySelectorAll('.images');
+let background = document.querySelector(".background_black");
+let modal = document.querySelector('.modal');
+let close = document.querySelector('.close');
+
+images.forEach(function (element, index) {
+    element.onclick = function () {
+
+        modal.style.display = 'flex'
+        background.style.display = 'flex';
+        let images = modal.querySelector('img')
+        images.src = element.querySelector('.divImag').src
+        element.style.cursor = 'default'
+        images.style.overflow = 'scroll'
+        
+    }
+});
+
+close.onclick = function () {
+    modal.style.display = 'none';
+    background.style.display = 'none';
+}
